66c4e6d3dca5cc92f183053843ea4c25deea051d
autumn-ragland <raglanda998@gmail.com>
2019-09-13 10:55:40 -0500

    Graded 09-13-2019 10:55:21

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # frontend-jQuery2-cw
-
+#### You manage to get the correct functionality for the challenge and the hover to change the height but not the width. You did not need to create a css file to change the background color as you could grab the html classes with jQuery inside your javaScript file and change the color there. 1/2 Score: 3/5
 Use the provided HTML file to answer all of the provided JavaScript questions in the js.js file. The jQuery file is attached as well. Properly connect it to your HTML file.
--- js.js
+++ js.js
@@ -6,7 +6,7 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 /* Give everything with the class 'hello' a white background when you click on one of them*/
 let hello1 =$('.hello');
 console.log(hello1);
-
+//!! This is Not what was asked. I reccomend checking out online to how to grab it. 
 /* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
 let moveO =$('h1');
 $('h1').mouseenter(function () {
@@ -16,7 +16,7 @@ $('h1').mouseenter(function () {
     $('h1').mouseleave(function () {
         $('h1').css('height','60px');
         // $('h1').css('width','-30')
-    })
+    }) //!! You have this commented out to prevent the change. If you ever need assistance in understanding what's happening please ask 
 });
 // CHALLENGE
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
